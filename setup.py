a21ed491f8e41df7740556102ae812296dcb6823
Borken <berice@aliasrobotics.com>
2019-05-23 11:35:15 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ setup(
     description='A ROS2 subscriber fuzzing tool for ROS systems',
     keywords=['network', 'fuzzing', 'ros', 'ros2'],
     entry_points={
-        'console_scripts': ['ros2_fuzzer=ros2_fuzzer.ros_fuzzer:main'],
+        'console_scripts': ['ros2_fuzzer=ros_fuzzer.ros_fuzzer:main'],
     },
     install_requires=[
         'hypothesis==3.82',
         'attrs==19.1.0',
-        'enum34==1.1.6',
-        'psutil==5.6.2'
+        'numpy==1.16.3',
     ],
     include_package_data=True,
 )
